ab169e3f914ef267474620454e34cfec9b8e7a8d
waisti <20104517@post.au.dk>
2017-09-21 11:41:31 +0200

    server

---
+++ root.js
@@ -0,0 +1,35 @@
+var http = require("http");
+var request = require('request');
+var Gpio = require('onoff').Gpio,
+sensor = new Gpio(17, 'in', 'both');
+
+var port = 8686;
+
+
+http.createServer(function(req,res){
+	console.log('New incoming client request for ' + req.url);
+	res.writeHeader(200, {'Content-Type': 'application/json'});
+	switch(req.url) {
+		case '/temperature':
+			sensor.watch(function (err, value) {
+				if (err) exit(err);
+					console.log(value ? 'there is some one!' : 'not anymore!');
+					res.write('{"Result" :' + value ? 'there is some one!' : 'not anymore!');
+				});
+				function exit(err) {
+					if (err) console.log('An error occurred: ' + err);
+						sensor.unexport();
+						console.log('Bye, bye!')
+						process.exit();
+					}
+				process.on('SIGINT', exit);
+			break;
+		case '/light':
+			res.write('{"light" :' + '}');
+			break;
+		default:
+			res.write('{"hello" : "world"}');
+		}
+res.end();
+}).listen(port);
+console.log('Server listening on http://localhost:' + port);
