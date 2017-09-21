79dd1c4df61656cfde8f5726a8e5358565fb6c0c
betaraybrian <troels.ahrengor@gmail.com>
2017-09-21 09:57:08 +0200

    changed to js

--- milestone2.js
+++ milestone2.js
@@ -1,3 +1,19 @@
-// Require express and create an instance of it
-var express = require('express');
-var app = express();
+var onoff = require('onoff');
+var Gpio = onoff.Gpio,
+	led = new Gpio(4, 'out'),
+	interval;
+
+interval = setInterval(function () {
+	var value = (led.readSync() + 1) % 2;
+	led.write(value, function() {
+		console.log("Changed LED state to: " + value);
+	});
+}, 2000);
+
+process.on('SIGINT', function () {
+	clearInterval(interval);
+	led.writeSync(0);
+	led.unexport();
+	console.log('Bye, bye!');
+	process.exit();
+});
