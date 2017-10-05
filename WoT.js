1ad49a5ca95f979f4de0a5fc418bef701b43ab14
Troels Ahrengot <troels.ahrengot@gmail.com>
2017-10-05 13:01:41 +0200

    added logs

--- WoT.js
+++ WoT.js
@@ -105,7 +105,9 @@ process.on('SIGINT', function () {
 function startUp(){
 	sendConnectToNetwork(networkIP, networkPort, piIP + ":" + port + "/pi/sensors/infrared", 10, function(error, response, body){
 		peer = JSON.parse(body);
+		console.log(peer);
 		setTimeout(function(){
+			console.log('sending data ...');
 			SendStore(peer.IP, peer.Port, infraValue, piIP + ":" + port + "/pi/sensors/infrared", function(error, response, body){} )
 		}, 2000)
 	});
